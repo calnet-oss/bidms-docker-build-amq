a9bd4566c16472f9ac342e4c5a6d880fd81ace08
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2017-08-13 12:12:40 -0700

    clone/pull repo from doBuild.sh script

--- imageFiles/root/doBuild.sh
+++ imageFiles/root/doBuild.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-cd /root/src/activemq-broker-and-webconsole-overlay-war \
-  && mvn install \
-  && cd with-jstl-overlay \
-  && mvn package
+mkdir -p /root/src
+cd /root/src
+
+if [ ! -d activemq-broker-and-webconsole-overlay-war ]; then
+  git clone https://github.com/calnet-oss/activemq-broker-and-webconsole-overlay-war.git
+else
+  (cd activemq-broker-and-webconsole-overlay-war; git pull)
+fi
+git_exit_code=$?
+if [ $git_exit_code != 0 ]; then
+  echo "Cloning or pulling activemq-broker-and-webconsole-overlay-war failed"
+  exit $git_exit_code
+fi
+
+cd activemq-broker-and-webconsole-overlay-war \
+  && ./gradlew war \
+  && ./gradlew publish
